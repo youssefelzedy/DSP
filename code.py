8d20c09369f8227c8af81c884d5d714cbf921250
Youssef Elzedy <youssefelzedy90@gmail.com>
2024-05-20 21:26:05 +0300

    add function

---
+++ code.py
@@ -0,0 +1,64 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.signal as signal
+
+#Change Only this part
+B = [1, 1.655, 1.655, 1]  # البسط
+A = [1, -1.57, 1.264, -0.4]  # المقام
+
+# Using the tf2zpk function to convert the transfer function to zeros, poles, and gain.
+plt.figure(figsize=(4, 4))
+z, p, k = signal.tf2zpk(B, A)
+plt.plot(np.real(z), np.imag(z), 'bo', fillstyle='none', markersize=10)
+plt.plot(np.real(p), np.imag(p), 'rx', markersize=10)
+unit_circle = plt.Circle((0, 0), 1, color='black', fill=False)
+plt.gca().add_artist(unit_circle)
+plt.xlim([-1.5, 1.5])
+plt.ylim([-1.5, 1.5])
+plt.xlabel('Real')
+plt.ylabel('Imaginary')
+plt.title('Poles and Zeros')
+plt.grid(True)
+
+# Without using the tf2zpk function.
+zeros = np.roots(B)
+poles = np.roots(A)
+plt.figure(figsize=(4, 4))
+plt.plot(np.real(zeros), np.imag(zeros), 'bo', fillstyle='none', markersize=10)
+plt.plot(np.real(poles), np.imag(poles), 'rx', markersize=10)
+unit_circle = plt.Circle((0, 0), 1, color='black', fill=False)
+plt.gca().add_artist(unit_circle)
+plt.xlim([-1.5, 1.5])
+plt.ylim([-1.5, 1.5])
+plt.xlabel('Real')
+plt.ylabel('Imaginary')
+plt.title('Poles and Zeros')
+plt.grid(True)
+
+# Frequency response, both amplitude and phase.
+plt.figure(figsize=(6, 3))
+w, h = signal.freqz(B, A)
+plt.plot(w, abs(h))
+plt.xlabel('Frequency [rad/sample]')
+plt.ylabel('Amplitude')
+plt.title('Frequency Response - Amplitude')
+plt.grid(True)
+
+plt.figure(figsize=(6, 3))
+plt.plot(w, np.angle(h))
+plt.xlabel('Frequency [rad/sample]')
+plt.ylabel('Phase')
+plt.title('Frequency Response - Phase')
+plt.grid(True)
+
+# Impulse response
+t, h = signal.dimpulse((B, A, 1), n=1000)
+h = np.squeeze(h)
+plt.figure(figsize=(6, 3))
+plt.plot(t, h)
+plt.xlabel('Time [samples]')
+plt.ylabel('Impulse response system')
+plt.title('Impulse response')
+
+
+plt.show()
